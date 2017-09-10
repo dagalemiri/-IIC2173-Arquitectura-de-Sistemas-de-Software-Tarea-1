a5c123ce3329d189965ff1a7ab866e4efa9e5b74
David Galemiri <dagalemiri@uc.cl>
2017-09-10 19:34:57 -0300

    Finished

--- controllers/requests.js
+++ controllers/requests.js
@@ -2,6 +2,8 @@
 
 let express = require('express');
 let router = express.Router();
+let dateFormat = require('dateformat');
+let Address6 = require('ip-address').Address6;
 
 let db = require('../db');
 let requestsModel = require('../models/requests')
@@ -9,8 +11,14 @@ let requestsModel = require('../models/requests')
 router.get('/', (req,res) => {
     let collection = db.get().collection('requests');
     let ip = req.connection.remoteAddress;
-    let date = Date.now();
-    let message =  {ip: ip, date: date};
+
+    let currentdate = new Date(); 
+    let datetime = dateFormat(currentdate);
+
+    let clientAddress = new Address6(ip);                    
+    let client_ip = clientAddress.to4().correctForm();
+
+    let message =  {ip: client_ip, date: datetime};
 
     requestsModel.append(message);
 
